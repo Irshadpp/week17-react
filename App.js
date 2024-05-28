6348b27da4085ef174421020a7c803b9fe29beca
Irshad <irshad89434@gmail.com>
2024-05-28 19:36:43 +0530

    laying the foundation

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading = React.createElement("h1", { id: "title" }, "Hello world");
+// const heading = <h1 className='heading'>Hello world</h1>
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(heading);
